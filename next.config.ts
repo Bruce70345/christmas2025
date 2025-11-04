fb7e14974cd22678fba19e559ce3726af0da372c
Bruce <bruce70345@gmail.com>
2025-11-05 00:09:40 +0800

    feat: 🎸 banner page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": ["@svgr/webpack"],
+    },
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/sections/banner.tsx
+++ src/sections/banner.tsx
@@ -1,7 +1,40 @@
+import Image from "next/image";
+
 export default function Banner() {
   return (
     <section className="banner-section relative grid min-h-[100svh] place-items-center overflow-hidden px-[6vw] pb-[8vh] pt-[12vh] text-center md:px-[8vw] md:pb-[10vh] md:pt-[14vh] lg:px-[6vw] lg:pb-[8vh] lg:pt-[12vh]">
-      <p className="absolute right-[5%] top-[20%] text-right text-[10vw] md:text-[7vw] lg:text-[7vw] lg:top-[45%] lg:-translate-y-1/2 max-text-[200px]">
+      <Image
+        src="/graphs/ce5f6256-c8b5-47b5-94bd-80af36580d91.png"
+        alt="Illustrated gathering of Greek gods"
+        width={2400}
+        height={2361}
+        priority
+        className="z-0 absolute w-[100vw] xs:w-[60vw] bottom-[10%] md:w-[40vw] md:left-[10%]"
+      />
+      <Image
+        src="/graphs/moon.svg"
+        alt="Crescent moon"
+        width={124}
+        height={118}
+        className="absolute left-[20%] top-[10vh] z-[5] w-[26vw] min-w-[6rem] max-w-[11rem] md:left-[70%] md:top-[12vh] md:w-[18vw] md:max-w-[10rem] lg:left-[70%] lg:top-[10vh]"
+      />
+      <Image
+        src="/graphs/cloud.png"
+        alt=""
+        width={262}
+        height={63}
+        aria-hidden="true"
+        className="absolute right-[45%] top-[12vh] z-[4] w-[30vw] max-w-[13rem] md:right-[15%] md:top-[14vh] md:w-[18vw]"
+      />
+      <Image
+        src="/graphs/cloud.png"
+        alt=""
+        width={262}
+        height={63}
+        aria-hidden="true"
+        className="absolute left-[5%] top-[18vh] z-[10] w-[36vw] max-w-[16rem] md:left-[60%] md:top-[20vh] md:w-[22vw]"
+      />
+      <p className="absolute right-[5%] top-[20%] z-10 text-right text-[10vw] md:text-[7vw] lg:text-[7vw] lg:top-[40%] lg:-translate-y-1/2 max-text-[200px]">
         <span
           className="layered-text banner-kicker"
           data-text="W/ BRUCE"
@@ -10,7 +43,7 @@ export default function Banner() {
         </span>
       </p>
       <h1
-        className="absolute left-[50%] top-[30%] w-full -translate-x-1/2 text-center text-[18vw] uppercase md:text-[22vw] lg:bottom-[10%] lg:top-auto lg:left-[50%] lg:max-w-none lg:-translate-x-1/2 lg:text-[14vw]"
+        className="absolute left-[50%] top-[25%] z-10 w-full -translate-x-1/2 text-center text-[18vw] uppercase md:text-[14vw] lg:bottom-[15%] lg:top-auto lg:left-[50%] lg:text-[14vw]"
         data-text="CHRISTMAS"
       >
         <span className="layered-text banner-title" data-text="CHRISTMAS">
